48f12a4820cb1af5ab0decf1650f394370e84b05
Sebastian Boerakker <basti@boerakker.com>
2018-12-14 16:53:17 +0100

    add Model Types for AC Quantum Api Response

---
+++ Model.py
@@ -0,0 +1,148 @@
+from typing import Any
+
+
+class AcRequestError(Exception):
+    def __init__(self, message, status_code=None):
+        # type: (str, int) -> None
+        self.status_code = status_code
+        self.message = message
+
+    def __str__(self):
+        return 'AcRequestError: \'{}\''.format(self.message)
+
+    def __repr__(self):
+        return 'AcRequestError: \'{}\''.format(self.message)
+
+
+class AcRequestForbiddenError(AcRequestError):
+    def __init__(self, message='403 Forbidden'):
+        # type: (str) -> None
+        self.message = message
+
+    def __str__(self):
+        return '403 - AcRequestForbiddenError: \'{}\''.format(self.message)
+
+    def __repr__(self):
+        return '403 - AcRequestForbiddenError: \'{}\''.format(self.message)
+
+
+class AcResponse:
+    def __init__(self, success=True, data=None, exception=None):
+        # type: (bool,Any, Any) -> None
+        self.success = success
+        self.data = data
+        self.exception = exception
+
+    def __str__(self):
+        return 'AcResponse: {{ success: {}, exception: {} }}'.format(self.success, self.exception)
+
+    def __repr__(self):
+        return 'AcResponse: {{ success: {}, exception: {} }}'.format(self.success, self.exception)
+
+
+class AcErrorResponse(object, AcResponse):
+
+    def __init__(self, success=False, exception=None, status_code=None):
+        # type: (bool, Any, int) -> None
+        super(AcErrorResponse, self).__init__()
+        self.status_code = status_code
+        self.success = success
+        self.exception = exception
+
+    def __str__(self):
+        return 'AcErrorResponse: {{ success: {}, exception: {}, status_code: {} }}'.format(self.success, self.exception,
+                                                                                           self.status_code)
+
+    def __repr__(self):
+        return 'AcErrorResponse: {{ success: {}, exception: {}, status_code: {} }}'.format(self.success, self.exception,
+                                                                                           self.status_code)
+
+
+class AcExperimentDetail:
+
+    def __init__(self, name, version, experiment_id, experiment_type, execution, bit_width=None):
+        # type: (str, int, int, str, int, int) -> None
+        self.name = name
+        self.version = version
+        self.experiment_id = experiment_id
+        self.experiment_type = experiment_type
+        self.execution = execution
+        self.bit_width = bit_width
+
+    def __str__(self):
+        return 'AcExperimentDetail: {{ name: {}, version: {}, experiment_id: {}, experiment_type: {}, execution: {}, ' \
+               'bit_width {} }}' \
+            .format(self.name, self.version, self.experiment_id, self.experiment_type, self.execution, self.bit_width)
+
+    def __repr__(self):
+        return 'AcExperimentDetail: {{ name: {}, version: {}, experiment_id: {}, experiment_type: {}, execution: {}, ' \
+               'bit_width {} }}' \
+            .format(self.name, self.version, self.experiment_id, self.experiment_type, self.execution, self.bit_width)
+
+
+class AcExperiment:
+
+    def __init__(self, detail, data, code=''):
+        # type: (AcExperimentDetail, Any, str) -> None
+        self.detail = detail
+        self.data = data
+        self.code = code
+
+    def __str__(self):
+        return 'AcExperiment: {{ data: {}, code: {}, detail: {} }}'.format(self.data, self.code, self.detail)
+
+    def __repr__(self):
+        return 'AcExperiment: {{ data: {}, code: {}, detail: {} }}'.format(self.data, self.code, self.detail)
+
+
+class AcResultResponse:
+
+    def __init__(self, simulated_result=None, real_result=None):
+        # type: ([AcResult], [AcResult]) -> None
+        self.simulated_result = simulated_result
+        self.real_result = real_result
+
+    def get_result(self):
+        # type: () -> [AcResult] or ([AcResult], [AcResult])
+        if self.simulated_result:
+            if self.real_result:
+                return self.real_result, self.simulated_result
+            return self.simulated_result
+        else:
+            return self.real_result
+
+    def __str__(self):
+        return 'AcResultResponse {{ simulated_result: {}, real_result: {} }}'.format(self.simulated_result,
+                                                                                     self.real_result)
+
+    def __repr__(self):
+        return 'AcResultResponse {{ simulated_result: {}, real_result: {} }}'.format(self.simulated_result,
+                                                                                     self.real_result)
+
+
+class AcResult:
+
+    def __init__(self, result_id, seed, shots, start_time, measure_qubits, finish_time=None, process=None, data=None,
+                 exception=None):
+        # type: (int, int, int, str, [int], str, str, dict, Any) -> None
+        self.result_id = result_id
+        self.seed = seed
+        self.shots = shots
+        self.start_time = start_time
+        self.measure_qubits = measure_qubits
+        self.finish_time = finish_time
+        self.process = process
+        self.data = data
+        self.exception = exception
+
+    def __str__(self):
+        return 'AcResult: {{ result_id: {}, seed: {}, shots: {}, start_time: {}, measure_qubits: {},  ' \
+               'finish_time: {}, process: {}, data: {} }} '.format(self.result_id, self.seed, self.shots,
+                                                                   self.start_time, self.measure_qubits,
+                                                                   self.finish_time, self.process, self.data)
+
+    def __repr__(self):
+        return 'AcResult: {{ result_id: {}, seed: {}, shots: {}, start_time: {}, measure_qubits: {}, ' \
+               'finish_time: {}, process: {}, data: {} }} '.format(self.result_id, self.seed, self.shots,
+                                                                   self.start_time, self.measure_qubits,
+                                                                   self.finish_time, self.process, self.data)
--- alibabaQuantum.py
+++ alibabaQuantum.py
@@ -1,10 +1,11 @@
 import pickle
 import re
+import requests
 from typing import List, Any
 
-import requests
-
 from Gates import Gate
+from Model import AcResponse, AcErrorResponse, AcExperiment, AcExperimentDetail, AcRequestError, \
+    AcRequestForbiddenError, AcResult, AcResultResponse
 
 
 class AcExperimentType(object):
@@ -89,7 +90,7 @@ class AlibabaQuantum(object):
         return self._req.post(uri, data=payload, params=params, headers=headers)
 
     def create_experiment(self, bit_width, experiment_type, experiment_name):
-        # type: (int, str, str) -> requests.Response
+        # type: (int, str, str) -> int
         uri = '{}/experiment/infosave'.format(self._base_uri)
         params = {self._CHARSET_PARAM[0]: self._CHARSET_PARAM[1]}
         headers = {self._TOKEN_HEADER_KEY: self._session.csrf, 'Content-Type': 'application/json'}
@@ -98,30 +99,30 @@ class AlibabaQuantum(object):
             'type': experiment_type,
             'name': experiment_name
         }
-        return self._req.post(uri, params=params, headers=headers, json=payload)
+        response = self.handle_ac_response(self._req.post(uri, params=params, headers=headers, json=payload))
+        return response.data
 
     def update_experiment(self, experiment_id, gates, code=None, override=True):
-        # type: (str, List[Gate], str, bool) -> requests.Response
+        # type: (int, List[Gate], str, bool) -> None
         uri = '{}/experiment/codesave'.format(self._base_uri)
         params = {
             self._CHARSET_PARAM[0]: self._CHARSET_PARAM[1]
         }
         headers = {self._TOKEN_HEADER_KEY: self._session.csrf, 'Content-Type': 'application/json'}
         payload = {
-            'experimentId': experiment_id,
+            'experimentId': str(experiment_id),
             'data': [gate.__dict__ for gate in gates],
             'code': code if code else ''
         }
         if not override:
-            response = self.get_experiment(experiment_id)
-            response = response.json()
-            existing_gates = response['data']['data']
+            exp = self.get_experiment(experiment_id)
+            existing_gates = exp.data
             payload['data'] = payload['data'] + existing_gates
 
-        return self._req.post(uri, json=payload, params=params, headers=headers)
+        self.handle_ac_response(self._req.post(uri, json=payload, params=params, headers=headers))
 
     def get_experiment(self, experiment_id):
-        # type: (str) -> requests.Response
+        # type: (int) -> AcExperiment or AcErrorResponse
         uri = '{}/experiment/detail'.format(self._base_uri)
         params = {
             'experimentId': experiment_id,
@@ -129,40 +130,30 @@ class AlibabaQuantum(object):
         }
         headers = {self._TOKEN_HEADER_KEY: self._session.csrf}
 
-        return self._req.get(uri, params=params, headers=headers)
+        response = self.handle_ac_response(self._req.get(uri, params=params, headers=headers))
+        body = response.data
+        exp_detail = AcExperimentDetail(body['experimentName'], body['version'], int(experiment_id),
+                                        body['experimentType'], body['execution'], bit_width=body['bitWidth'])
+        experiment = AcExperiment(detail=exp_detail, data=body['data'], code=body['code'])
+        return experiment
 
     def get_experiments(self):
-        # type: () -> requests.Response
+        # type: () -> [AcExperimentDetail] or AcErrorResponse
         uri = '{}/experiment/list'.format(self._base_uri)
         params = {
             self._CHARSET_PARAM[0]: self._CHARSET_PARAM[1]
         }
         headers = {self._TOKEN_HEADER_KEY: self._session.csrf, 'Content-Type': 'application/json'}
 
-        return self._req.get(uri, params=params, headers=headers)
-
-    def get_result(self, experiment_id):
-        # type: (str) -> requests.Response
-        uri = '{}/experiment/resultlist'.format(self._base_uri)
-        params = {
-            'experimentId': experiment_id,
-            self._CHARSET_PARAM[0]: self._CHARSET_PARAM[1]
-        }
-        headers = {self._TOKEN_HEADER_KEY: self._session.csrf}
-
-        return self._req.get(uri, params=params, headers=headers)
-
-    def download_result(self, experiment_id):
-        # type: (str) -> None
-        uri = '{}/experiment/resultDownload'.format(self._base_uri)
-        params = {'id': experiment_id}
-        headers = {self._TOKEN_HEADER_KEY: self._session.csrf}
-        request = self._req.get(uri, params=params, headers=headers)
-        with open('result-{}'.format(experiment_id), 'wb') as f:
-            f.write(request.content)
+        response = self.handle_ac_response(self._req.get(uri, params=params, headers=headers))
+        body = response.data
+        experiment_list = [
+            AcExperimentDetail(exp['name'], exp['version'], exp['experimentId'], exp['type'],
+                               exp['execution']) for exp in body]
+        return experiment_list
 
     def run_experiment(self, experiment_id, experiment_type, bit_width, shots, seed=None):
-        # type: (str, str, int, int, str) -> requests.Response
+        # type: (int, str, int, int, str) -> None
         uri = '{}/experiment/submit'.format(self._base_uri)
         headers = {'Content-Type': 'application/json', self._TOKEN_HEADER_KEY: self._session.csrf}
         params = {
@@ -173,7 +164,41 @@ class AlibabaQuantum(object):
             'shots': shots,
             'seed': seed if seed else ''
         }
-        return self._req.post(uri, headers=headers, params=params)
+        self.handle_ac_response(self._req.post(uri, headers=headers, params=params))
+
+    def get_result(self, experiment_id):
+        # type: (int) -> AcResultResponse
+        uri = '{}/experiment/resultlist'.format(self._base_uri)
+        params = {
+            'experimentId': experiment_id,
+            self._CHARSET_PARAM[0]: self._CHARSET_PARAM[1]
+        }
+        headers = {self._TOKEN_HEADER_KEY: self._session.csrf}
+
+        response = self.handle_ac_response(self._req.get(uri, params=params, headers=headers))
+        body = response.data
+        simulated_result = [
+            AcResult(res['id'], res['seed'], res['shots'], res['startTime'], res['measureQubits'], res['finishTime'],
+                     res['process'], res['data']) for res in body['simulateResult']]
+        real_result = [
+            AcResult(res['id'], res['seed'], res['shots'], res['startTime'], res['measureQubits'], res['finishTime'],
+                     res['process'], res['data']) for res in body['realResult']]
+        return AcResultResponse(simulated_result, real_result)
+
+    def download_result(self, experiment_id, file_name=None):
+        # type: (int, str) -> None
+        uri = '{}/experiment/resultDownload'.format(self._base_uri)
+        params = {'id': experiment_id}
+        headers = {
+            self._TOKEN_HEADER_KEY: self._session.csrf,
+            'Accept-Encoding': 'gzip, deflate'
+        }
+        response = self._req.get(uri, params=params, headers=headers)
+        if not file_name:
+            file_name = 'result_{}.xls'.format(experiment_id)
+
+        with open(file_name, 'wb') as f:
+            f.write(response.content)
 
     def _request_csrf_token(self):
         # type: () -> str
@@ -182,7 +207,7 @@ class AlibabaQuantum(object):
         return re.search('[a-f\d]{8}(-[a-f\d]{4}){3}-[a-f\d]{12}?', response.text).group()
 
     def delete_experiment(self, experiment_id):
-        # type: (str) -> requests.Response
+        # type: (int) -> None
         uri = '{}/experiment/delete'.format(self._base_uri)
         headers = {'Content-Type': 'application/json', self._TOKEN_HEADER_KEY: self._session.csrf}
         params = {
@@ -190,4 +215,30 @@ class AlibabaQuantum(object):
             'experimentId': experiment_id,
         }
 
-        return self._req.post(uri, headers=headers, params=params)
+        self.handle_ac_response(self._req.post(uri, headers=headers, params=params))
+
+    def handle_ac_response(self, response):
+        # type: (requests.Response) -> AcResponse
+        r"""
+        :param response: requests.Response
+        :return: AcResponse
+        :raises: AcRequestError, AcRequestForbiddenError
+        """
+        try:
+            if response.status_code == 200:
+                json = response.json()
+                if json['success']:
+                    try:
+                        data = json['data']
+                        return AcResponse(success=json['success'], exception=json['exception'], data=data)
+                    except KeyError:
+                        return AcResponse(success=json['success'], exception=json['exception'])
+                else:
+                    raise AcRequestError(json['exception'])
+            else:
+                error = response.json()
+                if response.status_code == 403:
+                    raise AcRequestForbiddenError()
+                raise AcRequestError(error['exception'], status_code=response.status_code)
+        except ValueError as err:
+            raise AcRequestError(err.__str__())
