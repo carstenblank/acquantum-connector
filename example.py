8c706ecdcbba5601202eaaa3749e84c4f1b2bb0a
Sebastian Boerakker <basti@boerakker.com>
2018-12-14 16:54:08 +0100

    update example on new Model Types

--- example.py
+++ example.py
@@ -6,20 +6,28 @@ api = AlibabaQuantum()
 api.create_session(AcCredentials('sebboer', 'qnpwzHyIIFw33Nw2PBx'))
 
 # Create Experiment
-response = api.create_experiment(bit_width=4, experiment_type=AcExperimentType.SIMULATE, experiment_name='Demo').json()
-experiment_id = response['data']
+experiment_id = api.create_experiment(bit_width=4, experiment_type=AcExperimentType.SIMULATE, experiment_name='Demo')
+print(experiment_id)
 
 # Update Experiment
 gates = [XGate(1, 1), Measure(2, 1)]
-response = api.update_experiment(experiment_id, gates).json()
+api.update_experiment(experiment_id, gates)
 
-# List Experiments
-exp_list = api.get_experiments().json()
-print(exp_list)
-
-exp_res = api.get_result(experiment_id).json()
+# Get Experiment
+exp_res = api.get_experiment(experiment_id)
 print(exp_res)
 
-# api.delete_experiment(experiment_id)
+# List Experiments
+exp_list = api.get_experiments()
+print(exp_list)
+
+# Run Experiment
+api.run_experiment(experiment_id, AcExperimentType.SIMULATE, 2, 100)
+
+# Get Result
+api.get_result(experiment_id)
+
+# Download Result
+api.download_result(experiment_id)
 
 api.save_session()
