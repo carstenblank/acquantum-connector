192a4cd8862aea81786dba2974f6a7c46b8842ac
Sebastian Boerakker <basti@boerakker.com>
2019-02-08 13:38:10 +0100

    fix 2 blank lines warning

--- acquantumconnector/model/errors.py
+++ acquantumconnector/model/errors.py
@@ -12,6 +12,7 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
+
 class AcQuantumRequestError(Exception):
     def __init__(self, message, status_code=None):
         # type: (str, int) -> None
